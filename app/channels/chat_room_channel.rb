07bf4f1945613261a2d2a0b60161cba476571d76
Shun Furukawa <furukawa9312@gmail.com>
2019-06-21 13:51:28 +0900

    :octocat: ADD generate channel chat_room

---
+++ app/channels/chat_room_channel.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ChatRoomChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@
 
 Rails.application.routes.draw do
   root 'chat_rooms#show'
+
+  mount ActionCable.server => '/cable'
 end
