080a2ca5fbd603415557856a1789bb24666fe94d
Shun Furukawa <furukawa9312@gmail.com>
2019-06-23 03:54:09 +0900

    ADD send chat message action

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,11 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+
+
+function postChatMessage() {
+  event.preventDefault();
+  var element = document.querySelector('input[type="text"]');
+  App.chat_room.speak(element.value);
+  element.value = '';
+}
